91df3b0d9a831444c8c7e3eaf5b1d839ec8e0b41
João Vitor Brandão <jvbrandaom@gmail.com>
2017-03-11 18:26:59 -0300

    Implements algorithm to calculate the triangle maximum total from top to bottom and creates a unit test for invalid inputs

--- HellTriangle.py
+++ HellTriangle.py
@@ -1,6 +1,25 @@
-def sum_triangle(triangle):
-    return 0
+def hell_triangle(triangle):
+    try:
+        return calculate_maximum_total(triangle)
+    except IndexError:
+        print("Invalid triangle input")
+
+
+def calculate_maximum_total(triangle):
+    index = max_index = total = 0
+    first_row = triangle.pop(0)
+    total += first_row[0]
+    for row in triangle:
+        if row[index] > row[index + 1]:
+            max_index = index
+        else:
+            max_index = index + 1
+        total += row[max_index]
+    return total
 
 
 def main():
-    return sum_triangle([])
+    print(hell_triangle([[6], [3, 5], [9, 7, 1], [4, 6, 8, 4]]))
+
+
+main()
--- TestHellTriangle.py
+++ TestHellTriangle.py
@@ -5,5 +5,10 @@ import HellTriangle
 class TestHellTriangle(unittest.TestCase):
     def test_hell_triangle(self):
         triangle = [[6], [3, 5], [9, 7, 1], [4, 6, 8, 4]]
-        result = HellTriangle.sum_triangle(triangle)
+        result = HellTriangle.calculate_maximum_total(triangle)
         self.assertEqual(result, 26)
+
+    def test_invalid_input(self):
+        triangle = [[6], [3], [9, 7, 1], [4, 6, 8, 4]]
+        with self.assertRaises(IndexError):
+            HellTriangle.calculate_maximum_total(triangle)
